d84e157cf99510672087e3363576428da38623e5
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-05-22 10:20:49 +0000

    Added some JavaScript

---
+++ script.js
@@ -0,0 +1,56 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeniners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") == "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+function runGame() {
+
+    // Generate two random numbers between 1 and 25
+    //Math.floor rounds down to the whole number
+    //Math.random generates random number
+
+    let num1 = Math.floor(Math.random() * 25 + 1);
+    let num2 = Math.floor(Math.random() * 25 + 1);
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswers() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
